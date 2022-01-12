1436541530292b25d442b2adbc362a4bf86fc78c
System Administrator <root@Toms-MBP.lan>
2022-01-12 08:30:48 +0000

    new one2

--- pages/_app.js
+++ pages/_app.js
@@ -8,17 +8,6 @@ import '../styles/globals.css';
 import { useRouter } from 'next/router';
 
 class MyApp extends App {
-  static getInitialProps = wrapper.getInitialAppProps(
-    (store) => async (context) => {
-      return {
-        pageProps: {
-          // https://nextjs.org/docs/advanced-features/custom-app#caveats
-          ...(await App.getInitialProps(context)).pageProps
-        }
-      };
-    }
-  );
-
   render() {
     const { Component, pageProps } = this.props;
 
